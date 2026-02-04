043c67c957fe3f30737170e4460a1982ae243ea1
yudasc <ydhnwb@gmail.com>
2026-02-04 22:38:58 +0700

    Add readme and fix error

--- UTS_PV_240401010353_Prieyuda/obj/Release/net10.0-windows/UAS_PV_240401010353_Prieyuda.AssemblyInfo.vb
+++ UTS_PV_240401010353_Prieyuda/obj/Release/net10.0-windows/UAS_PV_240401010353_Prieyuda.AssemblyInfo.vb
@@ -14,7 +14,7 @@ Option Explicit On
 Imports System
 Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("UAS_PV_240401010353_Prieyuda"),  _
- Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
+ Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
  Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986a8d71e8bc8fa1a78fd4b80bc281874c3b569d"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("UAS_PV_240401010353_Prieyuda"),  _
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
+## Untuk UAS
+file name nya beberapa masih UTS karena ini adalah melanjutkan project UTS dengan penambahan banyak fungsi sbg syarat UAS juga
+
+
 ## Visual Studio 2026
 
 Ini dibuat menggunakan visual studio 2026.
